8ed8f4e5de6b341fc91103c21483a5ecd1df9951
Patryck Henryck <patryckhenryck401@gmail.com>
2023-10-02 01:11:01 -0300

    feat(mochila.py): Correção de bugs, melhora da interface

--- Mochila.py
+++ Mochila.py
@@ -1,3 +1,23 @@
+
+#! Documentar o que cada função faz de forma mais detalhada
+
+#* Resolver bugs relacionados a números de base maior que 10 em pesos, valor, população, mochila.
+#* Aparentemente o erro relacionado a pesos é como o número maior que 10 está se comportando na lista
+#* O problema relacionado aos números maiores que 10 está inteiramente na definição do cromossomo e no uso de len para ler string
+#*pois o número 11 é reconhecido como 2 1s gerando assim um cromossomo erroneo pois terá dois genes
+#* Resolvido utilizando a conversão para lista inteira antes da definição da população inicial
+
+#! Existe um erro no crossover que acontece quando o cromossomo possui somente um gene
+
+#! Caso a mutação seja grande a casos de printar como resultado um cromossomo que não respeita a função fitness
+#!ou seja possui peso maior que permitido
+#! generalizando pois o erro pode não está somente na mutação o que aparentemente é verdade. 
+#! Há casos onde o cromossomo escolhido ultrapassa o limite da mochila
+
+#! Verificar como a taxa de mutação está se comportando e se está agindo corretamente
+
+#* Resolver erro aparecendo ao fechar ou cancelar: resolvido utilizando exit()
+
 from PySimpleGUI import PySimpleGUI as sg
 import random
 
@@ -5,6 +25,11 @@ sg.theme('Reddit')
 
 
 def converter_int(string):  # Converte os elementos que foram lidos como string na interface em uma lista de inteiros
+
+    '''
+    Converte um inteiro
+    '''
+
     lista_inteira = list()
     if ',' in string:
         string = string.replace(',', ' ')
@@ -13,13 +38,22 @@ def converter_int(string):  # Converte os elementos que foram lidos como string 
     for item in string:
         lista_inteira.append(int(item))
 
+    print(lista_inteira)
     return lista_inteira
 
 
-# Função de fitness
+#* Função onde recebe o cromossomo ler os genes e retorna o FITNESS
 def fitness(cromossomo):
+
+    '''
+    Converte um inteiro
+    '''
+
     soma_pesos = soma_valores = 0
     for d in range(len(cromossomo)):
+        print (d)
+        print (cromossomo)
+        print (pesos)
         if cromossomo[d] == 1:
             soma_pesos += pesos[d]
             soma_valores += valores[d]
@@ -32,6 +66,11 @@ def fitness(cromossomo):
 
 # Função de cruzamento genético
 def crossover(pai1, pai2):
+
+    '''
+    Converte um inteiro
+    '''
+
     ponto_corte = random.randint(1, len(pai1)-1)
     filho1 = pai1[:ponto_corte] + pai2[ponto_corte:]
     filho2 = pai2[:ponto_corte] + pai1[ponto_corte:]
@@ -40,6 +79,11 @@ def crossover(pai1, pai2):
 
 # Função de mutação
 def mutacao(individuo):
+
+    '''
+    Converte um inteiro
+    '''
+
     posicao = random.randint(0, len(individuo)-1)
     individuo[posicao] = not individuo[posicao]
     return individuo
@@ -70,7 +114,7 @@ while True:
 
     if e == sg.WINDOW_CLOSED or e == 'cancelar':
         janela_parametros.close()
-        break
+        exit()
     if e == 'continuar':
         pesos = v['pesos'][:]
         valores = v['valores'][:]
@@ -81,21 +125,49 @@ while True:
         janela_parametros.close()
         break
 
+#!Barra de progresso
+tela_aguarde = [
+    [sg.Text('Progresso')],
+    [sg.ProgressBar(geracoes, orientation='h', size=(20, 20), key='progressbar')]
+]
+
+Carregamento = sg.Window('Barra de Progresso', tela_aguarde)
+
+#! Janela de histórico
+log_layout = [
+    [sg.Multiline(size=(60, 10), key='-LOG-', autoscroll=True)]
+]
+
+janela_logs = sg.Window('LOG', layout=log_layout)
+
+log = []
+
 pesos = converter_int(pesos)
 valores = converter_int(valores)
 
 # Inicializando a população
+print(f'Printando pesos', pesos)
 populacao = [[random.choice([0, 1]) for _ in range(len(pesos))] for _ in range(tamanho_populacao)]
 
 # Algoritmo genético
+
 for geracao in range(geracoes):
-    populacao = sorted(populacao, key=lambda x: fitness(x), reverse=True)
+    log.append(f'populacao: {populacao} geracao: {geracao}')
+    print(f'populacao: {populacao} geracao: {geracao}')
+
+    event, values = Carregamento.read(timeout=0.00001)  # Adiciona timeout para evitar bloqueio
+    Carregamento.refresh()
+    if event == sg.WINDOW_CLOSED:
+        janela_parametros.close()
+        break
+
+    populacao = sorted(populacao, key=lambda x: fitness(x), reverse=1)
     nova_populacao = []
 
-    # Mantém os melhores cromossomos
+        # Mantém os melhores cromossomos
     nova_populacao.extend(populacao[:2])
 
-    # Geração de novos indivíduos através de crossover e mutação
+        # Geração de novos indivíduos através de crossover e mutação
     for _ in range(tamanho_populacao // 2):
         pai1, pai2 = random.choice(populacao[:5]), random.choice(populacao[:5])
         filho1, filho2 = crossover(pai1, pai2)
@@ -104,15 +176,19 @@ for geracao in range(geracoes):
         if random.random() < taxa_mutacao:
             filho2 = mutacao(filho2)
         nova_populacao.extend([filho1, filho2])
+        
+    Carregamento['progressbar'].update(geracao+1)
 
     populacao = nova_populacao
+Carregamento.close()
 
 
-# Obtendo o melhor cromossomo após as gerações
+
+    # Obtendo o melhor cromossomo após as gerações
 melhor_cromossomo = max(populacao, key=fitness)
 melhor_cromossomo = [1 if gene else 0 for gene in melhor_cromossomo]
 
-# Cálculo do total de peso e valor dos livros selecionados geneticamente
+    # Cálculo do total de peso e valor dos livros selecionados geneticamente
 valor_total = peso_total = 0
 for c in range(len(melhor_cromossomo)):
     if melhor_cromossomo[c] == 1:
@@ -124,14 +200,23 @@ tela_resultados = [
     [sg.Text(f'Melhor Indivíduo: {melhor_cromossomo}')],
     [sg.Text(f'Valor Total: {valor_total}')],
     [sg.Text(f'Peso Total: {peso_total}')],
-    [sg.Push(), sg.Button('Sair'), sg.Push()]
+    [sg.Push(), sg.Button('Sair'), sg.Push(),sg.Button('Mostrar/Esconder Log')],
+    [sg.Multiline(size=(60, 10), key='-LOG-',visible = False, autoscroll=True)]
+    #,[sg.Listbox(values=log, size=(30, len(log)), key='-LISTBOX-')],
 ]
 
 # Exibindo resultados
 janela_resultados = sg.Window('RESULTADOS', layout=tela_resultados)
-while True:
-    e, v = janela_resultados.read()
 
+
+log_visible = False
+
+while True:
+    e, v = janela_resultados.read(timeout = 0.1)
     if e == sg.WINDOW_CLOSED or e == 'Sair':
         janela_resultados.close()
         break
+    if e == 'Mostrar/Esconder Log':
+        log_visible = not log_visible
+        janela_resultados['-LOG-'].update(value='\n'.join(log), visible=log_visible)
+        janela_resultados.refresh()
